71602b507bd5154ba4fe84a571ddc21ffa528bc6
root <root@DESKTOP-SRI5HM6>
2024-08-25 21:59:53 +0700

    scrapping test

--- scrapping.py
+++ scrapping.py
@@ -10,7 +10,7 @@ from fuzzywuzzy import fuzz
 start_date = '2024-08-01'
 end_date = '2024-08-30'
 chromium_path = '/home/opt/chrommium'
-executable_file = 'chrome.exe'
+executable_file = 'chrome'
 executable_path = os.path.join(chromium_path, executable_file)
 headless_option = False
 sort_option = 'recent'
