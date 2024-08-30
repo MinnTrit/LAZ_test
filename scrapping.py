b5ca22cabe8735d4d25b6106aa2c3edacad58873
root <root@DESKTOP-SRI5HM6>
2024-08-30 23:15:18 +0700

    test for aug

--- scrapping.py
+++ scrapping.py
@@ -7,10 +7,10 @@ import pandas as pd
 import time
 from fuzzywuzzy import fuzz
 
-start_date = '2024-07-01'
-end_date = '2024-07-31'
+start_date = '2024-08-01'
+end_date = '2024-08-30'
 chromium_path = '/root/.cache/ms-playwright/chromium-1129/chrome-linux'
-executable_file = 'chrome'
+executable_file = 'chrome.exe'
 executable_path = os.path.join(chromium_path, executable_file)
 headless_option = False
 sort_option = 'recent'
